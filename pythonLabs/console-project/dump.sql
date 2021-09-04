5436d75332d145809c84eb5862c924bead8596d3
Mohamad Aboda <41721894+Mohamad-Aboda@users.noreply.github.com>
2021-09-04 16:25:42 +0200

    Update dump.sql

--- pythonLabs/console-project/dump.sql
+++ pythonLabs/console-project/dump.sql
@@ -2,6 +2,8 @@ CREATE DATABASE test;
 
 CREATE TABLE registration_info(fname VARCHAR(20), lname VARCHAR(20), email VARCHAR(40), user_password VARCHAR(40), phone VARCHAR(11))
 
+INSERT INTO registration_info VALUES('mohamed', 'abdelhamid', 'aboda@gmail.com', '123', '01234534535')
+
 CREATE TABLE project_info(title VARCHAR(20), details VARCHAR(100), total_target MONEY, start_date DATE, end_date DATE)
 
 create database
